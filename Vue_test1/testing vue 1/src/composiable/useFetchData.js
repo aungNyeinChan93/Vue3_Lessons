3cae0aeaa986cdd71480a38eccf4253ef1e8ed0f
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-11-28 00:52:32 +0630

    update

--- Vue_test1/testing vue 1/src/composiable/useFetchData.js
+++ Vue_test1/testing vue 1/src/composiable/useFetchData.js
@@ -6,7 +6,7 @@ export function useFetchData () {
     const products = ref([]);
     const productsFetch = async() => {
         const res  = await fetch('https://fakestoreapi.com/products')
-        products.value = await res.json()
+        products.value =  await res.json()
     }
 
     return  {
--- Vue_test1/testing vue 1/src/views/HomeView.vue
+++ Vue_test1/testing vue 1/src/views/HomeView.vue
@@ -19,8 +19,17 @@
 
     <h1 class="underline text-capitalize text-4xl text-center text-green-300 my-2">Men clothing</h1>
     <ul class="px-10">
-      <li v-for="men in categoryFilter(products).menClothing" :key="men.id">
+      <li v-for="men in menCloths(products)" :key="men.id">
         <h5 class="p-2 my-1 rounded bg-red-300 shadow">{{ men.title }}</h5>
+      </li>
+    </ul>
+
+<!--    title only-->
+    <h1 class="underline text-capitalize text-4xl text-center text-green-300 my-2">Title Only</h1>
+
+    <ul class="px-10">
+      <li v-for="t in productTitle" :key="t">
+        <h5 class="p-2 my-1 rounded bg-red-300 shadow">{{ t }}</h5>
       </li>
     </ul>
   </main>
@@ -32,20 +41,42 @@ import {useFetchData} from "@/composiable/useFetchData.js";
 import useTest from "../composiable/useTest.js";
 import Card from '@/components/Card.vue';
 import Test from "@/components/Test.vue";
-import { onMounted, ref} from "vue";
+import {computed, onMounted, onUpdated, ref} from "vue";
 
 const isLoadingStatus = ref(false);
 const {productsFetch, products} = useFetchData();
 const {test, alertName} = useTest();
+const titles= ref([]);
 
 const categoryFilter = (p) => {
   return {
-    menClothing :p.filter(product => product.category === 'men\'s clothing')
+    menClothing: p.filter(product => product.category === 'men\'s clothing')
   }
+};
+
+const menCloths = (products) => {
+  return categoryFilter(products).menClothing
 }
+
+const productTitle = computed(() => {
+  return titles.value = products.value.map(product => product.title);
+})
+
+
+// console.log(products.value)
+// console.log(menCloths);
+// console.log('hry',products.value.map(p=> p.title));
+// console.log(products.value.map(product => product.title));
+
 
 onMounted(() => {
   productsFetch();
+});
+
+onUpdated(() => {
+  titles.value =products.value.map(product => product.title);
+  console.log(titles.value)
+  // console.log(products.value.map(product => product.title));
 })
 
 
